6cbaf6642c64d63386d3327eb40d23ff15ea6f49
Anton Petrov <anton.petrov.swe@gmail.com>
2025-03-12 22:16:40 +0300

    Move `fish` and `tmux` config to `dotfiles` repo

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,41 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    if string match -q -- '*ghostty*' $TERM
+        set -g fish_vi_force_cursor 1
+    end
+
+    # Set up `fzf` key bindings (https://github.com/junegunn/fzf):
+    fzf --fish | source
+
+    # Set up `zoxide` integration with `fish` (hhttps://github.com/ajeetdsouza/zoxide)
+    zoxide init fish | source
+
+    # Enable VI-bindings
+    fish_vi_key_bindings
+    # Set cursor shapes for VI-modes
+    set fish_cursor_default block
+    set fish_cursor_insert line
+    set fish_cursor_replace_one underscore
+    set fish_cursor_replace underscore
+    set fish_cursor_external line
+    set fish_cursor_visual block
+end
+
+# Aliases
+alias lg="lazygit"
+
+alias gs="git status"
+alias gco="git checkout"
+alias gc="git commit"
+alias ga="git add"
+alias gb="git branch"
+alias gd="git diff"
+
+alias ls="lsd"
+alias l="ls -l"
+alias la="ls -la"
+alias lt="ls --tree"
+alias lta="ls -la --tree"
+
+# Set up shell completions for `uv`
+uv generate-shell-completion fish | source
