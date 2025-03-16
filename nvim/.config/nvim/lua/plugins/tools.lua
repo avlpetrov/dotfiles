6e9453607bb3c4793ad69b849a7647183e9cf32d
Anton Petrov <anton.petrov.swe@gmail.com>
2025-03-16 17:56:34 +0300

    Fix keymaps collision between tmux, dial and jumping forward

--- nvim/.config/nvim/lua/plugins/tools.lua
+++ nvim/.config/nvim/lua/plugins/tools.lua
@@ -11,12 +11,12 @@ return {
       },
     },
   },
-  {
-    "monaqa/dial.nvim",
-    -- Disable unused keymaps
-    keys = {
-      { "<C-a>", false },
-      { "g<C-a>", false },
-    },
-  },
+  -- {
+  --   "monaqa/dial.nvim",
+  --   -- Disable unused keymaps
+  --   keys = {
+  --     { "<C-a>", false },
+  --     { "g<C-a>", false },
+  --   },
+  -- },
 }
