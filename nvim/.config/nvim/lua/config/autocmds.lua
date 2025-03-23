b3e4c737796642c0aadc4a496faf30eeeb64ac2f
Anton Petrov <anton.petrov.swe@gmail.com>
2025-03-24 01:21:30 +0300

    Add relative line numbers to `mini.files`

--- nvim/.config/nvim/lua/config/autocmds.lua
+++ nvim/.config/nvim/lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Add relative line numbers to `mini.files`
+-- https://github.com/echasnovski/mini.nvim/issues/747
+vim.api.nvim_create_autocmd("User", {
+  pattern = "MiniFilesWindowUpdate",
+  callback = function(args)
+    local win = vim.wo[args.data.win_id]
+    win.number = true
+    win.relativenumber = true
+  end,
+})
