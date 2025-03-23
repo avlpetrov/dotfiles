4c53d080de9bbdfb38c3ac6f769576fde74cf6ef
Anton Petrov <anton.petrov.swe@gmail.com>
2025-03-24 00:49:17 +0300

    Use `mini.files` as a default explorer

--- nvim/.config/nvim/lua/plugins/files.lua
+++ nvim/.config/nvim/lua/plugins/files.lua
@@ -31,9 +31,7 @@ return {
       width_preview = 100,
     },
     options = {
-      -- Whether to use for editing directories
-      -- Disabled by default in LazyVim because neo-tree is used for that
-      use_as_default_explorer = false,
+      use_as_default_explorer = true,
     },
   },
 }
