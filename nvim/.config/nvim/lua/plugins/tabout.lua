9709dd80b66ddaaec3a7acb398ec80be49b33ac6
Anton Petrov <anton.petrov.swe@gmail.com>
2025-03-25 02:52:14 +0300

    Add `tabout` plugin to escape closing quotes/braces in Insert mode

---
+++ nvim/.config/nvim/lua/plugins/tabout.lua
@@ -0,0 +1,27 @@
+-- https://github.com/abecodes/tabout.nvim
+-- Allows to tabout from a closing parentheses, quotes, etc., while in Insert mode
+return {
+  {
+    "abecodes/tabout.nvim",
+    lazy = false,
+    config = function()
+      require("tabout").setup({
+        tabkey = "<Tab>", -- key to trigger tabout, set to an empty string to disable
+        act_as_tab = false, -- shift content if tab out is not possible
+        enable_backwards = false,
+        completion = false, -- if the tabkey is used in a completion pum
+        tabouts = {
+          { open = "'", close = "'" },
+          { open = '"', close = '"' },
+          { open = "`", close = "`" },
+          { open = "(", close = ")" },
+          { open = "[", close = "]" },
+          { open = "{", close = "}" },
+        },
+        ignore_beginning = true, --[[ if the cursor is at the beginning of a filled element it will rather tab out than shift the content ]]
+        exclude = {}, -- tabout will ignore these filetypes
+      })
+    end,
+    opt = false, -- Set this to true if the plugin is optional
+  },
+}
