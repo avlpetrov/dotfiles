357e01fb9e926ba3f686723baabcd248ce714a63
Anton Petrov <anton.petrov.swe@gmail.com>
2025-03-25 03:11:54 +0300

    Add autocompletion toggle

---
+++ nvim/.config/nvim/lua/plugins/cmp.lua
@@ -0,0 +1,38 @@
+-- Add Autocompletion Toggle keymap
+-- https://github.com/LazyVim/LazyVim/discussions/3302#discussioncomment-10042590
+return {
+  {
+    "hrsh7th/nvim-cmp",
+    init = function()
+      vim.g.cmp_disabled = false
+    end,
+    opts = function(_, opts)
+      opts.enabled = function()
+        -- local context = require("cmp.config.context")
+        if vim.g.cmp_disabled == true then
+          return false
+        end
+        -- some other conditions (like not in commments) can go here
+        return not disabled
+      end
+    end,
+    keys = {
+      {
+        "<leader>uk",
+        function()
+          vim.g.cmp_disabled = not vim.g.cmp_disabled
+          local msg = ""
+          if vim.g.cmp_disabled == true then
+            msg = "Autocompletion (cmp) disabled"
+          else
+            msg = "Autocompletion (cmp) enabled"
+          end
+          vim.notify(msg, vim.log.levels.INFO)
+        end,
+        noremap = true,
+        silent = true,
+        desc = "Toggle Autocompletion",
+      },
+    },
+  },
+}
