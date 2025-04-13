df090f203a4528962b9324e69c4eb1ff8fdaaf3c
Anton Petrov <anton.petrov.swe@gmail.com>
2025-04-13 17:44:17 +0300

    nvim: set a strict heading width for `render-markdown.nvim` to avoid it being too long and noisy

---
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,10 @@
+-- https://github.com/MeanderingProgrammer/render-markdown.nvim/wiki/
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  opts = {
+    heading = {
+      width = "block",
+      min_width = 30,
+    },
+  },
+}
