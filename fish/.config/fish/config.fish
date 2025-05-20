021844290c4445679b24ac5b204e802e485f7633
Anton Petrov <anton.petrov.swe@gmail.com>
2025-05-20 04:41:13 +0300

    fish: set `XDG_CONFIG_HOME` in fish way

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -25,7 +25,7 @@ if status is-interactive
     set fish_cursor_visual block
 end
 
-export XDG_CONFIG_HOME="$HOME/.config"
+set -Ux XDG_CONFIG_HOME "$HOME/.config"
 
 # Set NeoVim as the default editor
 set -gx EDITOR nvim
